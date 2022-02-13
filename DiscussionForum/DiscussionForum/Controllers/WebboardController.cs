322746049dd308a79afac4c48e8ef127f4b1fd98
Piyathida San-aoun <piyathida.s@draycir.com>
2022-02-13 17:17:05 +0700

    updated page for web board

---
+++ DiscussionForum/DiscussionForum/Controllers/WebboardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiscussionForum.Controllers
+{
+    public class WebboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
