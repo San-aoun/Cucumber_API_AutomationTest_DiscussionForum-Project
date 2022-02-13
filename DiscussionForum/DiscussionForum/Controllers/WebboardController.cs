6e15bad50d2e38399911091553ca22138d50b849
Piyathida San-aoun <piyathida.s@draycir.com>
2022-02-13 17:58:57 +0700

    set namespace model to use on view

--- DiscussionForum/DiscussionForum/Controllers/WebboardController.cs
+++ DiscussionForum/DiscussionForum/Controllers/WebboardController.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DiscussionForum.Models.db;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DiscussionForum.Controllers
 {
     public class WebboardController : Controller
     {
+        private readonly discussionForumDBContext _db;
+
+        public WebboardController(discussionForumDBContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
